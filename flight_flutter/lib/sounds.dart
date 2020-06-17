079f7bc72153d8a5919ddcc798ea66ebebffcc1c
nanduu04 <33555126+nanduu04@users.noreply.github.com>
2020-06-17 09:42:35 -0400

    removed explecit sounds

--- flight_flutter/lib/sounds.dart
+++ flight_flutter/lib/sounds.dart
@@ -4,22 +4,15 @@ class FlightSounds {
     'sounds/dolphin.mp3',
     'sounds/dunklios.mp3',
     'sounds/intro.mp3',
-    'sounds/irish.mp3',
     'sounds/lookAtCurry.mp3',
     'sounds/wontletyou.mp3',
     'sounds/nice.mp3',
-    'sounds/roast2.mp3',
     'sounds/intro.mp3',
-    'sounds/irish.mp3',
     'sounds/lookAtCurry.mp3',
     'sounds/nice.mp3',
-    'sounds/roast2.mp3',
     'sounds/intro.mp3',
-    'sounds/irish.mp3',
     'sounds/lookAtCurry.mp3',
     'sounds/nice.mp3',
-    'sounds/roast2.mp3',
-    
   ];
   get sounds => _sound;
 }
